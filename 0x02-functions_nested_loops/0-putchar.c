d0abdef0b7dd4855b2c5396dc21fddb32a7a9f16
raytaiwo <ray.taiwo@outlook.com>
2022-12-13 11:17:56 -0800

    task o commited

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,19 +3,19 @@
 /**
  * main - program that prints _putcher
  * Raycode
- * Return: int
+ * Return: 0
  */
 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
